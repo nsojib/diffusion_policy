10fd143f391e0cb5dfbd5d13b6681b50c4c0e3bd
nsojib <noushad.sust@gmail.com>
2025-05-16 10:16:28 -0400

    image h5py key

--- train.py
+++ train.py
@@ -34,3 +34,9 @@ def main(cfg: OmegaConf):
 
 if __name__ == "__main__":
     main()
+
+# python train.py --config-dir=. --config-name=config_lowdim_lift_mh.yaml training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
+# python train.py --config-dir=. --config-name=config_image_lift_mh.yaml training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
+
+# python train.py --config-dir=. --config-name=config_image_lift_mh.yaml +task.dataset.hdf5_filter_key="worse" training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
+
