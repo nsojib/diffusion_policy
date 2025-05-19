3a43c7233cd1a2a083607b2a633197247f317bd1
nsojib <noushad.sust@gmail.com>
2025-05-18 23:10:43 -0400

    t.save

--- train_save.sh
+++ train_save.sh
@@ -2,18 +2,18 @@ wandb offline
 wandb disabled
 
 
-training_path="data/outputs/pusht_img1"
+# training_path="data/outputs/pusht_img1"
 
-python train.py --config-dir="configs" \
-    --config-name=image_pusht_diffusion_policy_cnn.yaml \
-    training.seed=42 \
-    training.device=cuda:0 \
-    hydra.run.dir="$training_path"\
-    checkpoint.topk.k=21\
-    training.num_epochs=401 \
-    training.checkpoint_every=20 \
-    training.rollout_every=20 \
-    +save_rollout=true
+# python train.py --config-dir="configs" \
+#     --config-name=image_pusht_diffusion_policy_cnn.yaml \
+#     training.seed=42 \
+#     training.device=cuda:0 \
+#     hydra.run.dir="$training_path"\
+#     checkpoint.topk.k=21\
+#     training.num_epochs=401 \
+#     training.checkpoint_every=20 \
+#     training.rollout_every=20 \
+#     +save_rollout=true
 
 # training_path="data/outputs/pusht_lowdim1"
 
@@ -27,6 +27,22 @@ python train.py --config-dir="configs" \
 #     training.checkpoint_every=20 \
 #     training.rollout_every=20 \
 #     +save_rollout=true
+
+
+
+training_path="data/outputs/lift_mh_img1"
+
+python train.py --config-dir="configs" \
+    --config-name=config_image_lift_mh.yaml \
+    training.seed=42 \
+    training.device=cuda:0 \
+    hydra.run.dir="$training_path"\
+    checkpoint.topk.k=21\
+    training.num_epochs=401 \
+    training.checkpoint_every=20 \
+    training.rollout_every=20 \
+    +save_rollout=true \
+    +task.dataset.hdf5_filter_key="worse"
 
 
 python save_training_rollouts.py  --training_path "$training_path"
